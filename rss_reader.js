e0242317c2946b0684634972e3fbfa02e4e1d9c8
VukZh <36912856+VukZh@users.noreply.github.com>
2018-06-30 00:04:29 +0600

    Update rss_reader.js

--- rss_reader.js
+++ rss_reader.js
@@ -9,31 +9,40 @@ let parser = new Parser();
 
 let feed;
 (async () => {
-    feed = await parser.parseURL('https://lenta.ru/rss/last24');
-    console.log('RSS     - ', feed.title);
-    console.log('Records - ', feed.items.length);
 
-    await app.get('/rss', function (req, res) {
-        if (err) return res.status(400).json({
-            error: err.message
+    try {
+        feed = await parser.parseURL('https://lenta.ru/rss/last24');
+        console.log('RSS     - ', feed.title);
+        console.log('Records - ', feed.items.length);
+
+        app.get('/rss', function (req, res) {
+            if (err) return res.status(400).json({
+                error: err.message
+            });
+            res.send(feed.items);
         });
-        res.send(feed.items);
-    });
+
+        MongoClient.connect("mongodb://localhost:27017",
+            function (err, database) {
+                if (err) return console.log('*Error*', err);
+
+                let dbo = database.db("db");
+                dbo.collection("RSS").remove(); //remove collection
+                dbo.collection("RSS").insertMany(feed.items, function (err, res) {
+                    if (err) throw err;
+                    // console.log(res);
+                    console.log('RSS insert');
+                    database.close();
+                    process.exit(-1);
+                });
+            });
+
+    } catch (error) {
+        console.log('*Error connect*', error);
+        process.exit(-1);
+    };
 
 })();
-
-MongoClient.connect("mongodb://localhost:27017",
-    function (err, database) {
-        if (err) return console.log('*Error*', err);
-        let dbo = database.db("db");
-        dbo.collection("RSS").remove(); //remove collection
-        dbo.collection("RSS").insertMany(feed.items, function (err, res) {
-            if (err) throw err;
-            // console.log(res);
-            console.log('RSS insert');
-            database.close();
-        });
-    });
 
 app.listen(port, function () {
     console.log('We are live on ' + port);
